b74170accf94da61597ddaad6204aa6fb344f2c6
Marek <marek.b@cstechnologies.cz>
2020-04-20 13:22:13 +0200

    dialog drag

--- BlazorComponents/DialogHost.razor.cs
+++ BlazorComponents/DialogHost.razor.cs
@@ -1,17 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
 namespace BlazorComponents
 {
+    public class DialogContext
+    {
+        private readonly DialogsService _Service;
+
+        public DialogContext(DialogsService service, DialogInfo info, RenderFragment childContent) : this(info) {
+            _Service = service;
+            ChildContent = childContent;
+            OnActionExecuted = service.OnActionExecuted;
+        }
+
+        private DialogContext(DialogInfo info) {
+            Identifier = info.Identifier;
+            Top = info.Top;
+            Left = info.Left;
+            Width = info.Width;
+            Height = info.Height;
+            Header = info.Header;
+            Actions = info.Actions;
+            CancelResult = info.CancelResult;
+            Modal = info.Modal;
+            Percents = info.Percents;
+            CloseButtonInactive = info.CloseButtonInactive;
+            Icon = info.Icon;
+        }
+
+        public bool Active {
+            get { return _Service.Dialogs.IndexOf(this) == 0; }
+        }
+
+        public RenderFragment ChildContent { get; }
+        public Action<string, string> OnActionExecuted { get; }
+
+        public int ZIndex {
+            get { return 5000 + _Service.Dialogs.IndexOf(this); }
+        }
+
+        public (string title, string result)[] Actions { get; set; }
+        public string CancelResult { get; set; }
+        public bool CloseButtonInactive { get; set; }
+        public string Header { get; set; }
+        public double Height { get; set; }
+        public string Icon { get; set; }
+
+        public string Identifier { get; set; }
+        public double Left { get; set; }
+        public bool Modal { get; set; }
+        public bool Percents { get; set; }
+        public double Top { get; set; }
+        public double Width { get; set; }
+    }
+
+    public abstract class DialogHostBase : ComponentBase, IDisposable
+    {
+        [Inject]
+        public DialogsService DialogsService { get; set; }
+
+        public void Dispose() {
+            DialogsService.Update -= DialogsServiceOnUpdate;
+        }
+
+        protected override void OnInitialized() {
+            base.OnInitialized();
+            DialogsService.Update += DialogsServiceOnUpdate;
+        }
+
+        private void DialogsServiceOnUpdate() {
+            StateHasChanged();
+        }
+    }
+
+    public class DialogInfo
+    {
+        public (string title, string result)[] Actions { get; set; }
+        public string CancelResult { get; set; }
+        public bool CloseButtonInactive { get; set; }
+        public string Header { get; set; }
+        public int Height { get; set; }
+        public string Icon { get; set; }
+        public string Identifier { get; set; }
+        public int Left { get; set; }
+        public bool Modal { get; set; }
+        public bool Percents { get; set; }
+        public int Top { get; set; }
+        public int Width { get; set; }
+    }
+
     public sealed class DialogsService
     {
+        public event Action<string, string> ActionExecuted;
         public event Action Update;
-        public event Action<(string identifier, string result)> ActionExecuted;
 
         public List<DialogContext> Dialogs { get; } = new List<DialogContext>();
+
+        public DialogContext FindDialogContext(string identifier) {
+            return Dialogs.Find(o => o.Identifier == identifier);
+        }
 
         public void ShowDialog<T>(DialogInfo info) where T : ComponentBase {
             ShowDialog(typeof(T), info);
@@ -33,99 +122,20 @@ namespace BlazorComponents
             }
         }
 
-        private void OnUpdate() {
-            Update?.Invoke();
-        }
-
-        internal void OnActionExecuted((string identifier, string result) item) {
+        internal void OnActionExecuted(string identifier, string result) {
             lock (Dialogs) {
-                int index = Dialogs.FindIndex(o => o.Identifier == item.identifier);
+                int index = Dialogs.FindIndex(o => o.Identifier == identifier);
                 if (index != -1) {
                     Dialogs.RemoveAt(index);
+                    OnUpdate();
                 }
             }
 
-            ActionExecuted?.Invoke(item);
-        }
-    }
-
-    public class DialogInfo
-    {
-        public string Identifier { get; set; }
-        public int Top { get; set; }
-        public int Left { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public string Header { get; set; }
-        public (string title, string result)[] Actions { get; set; }
-        public string CancelResult { get; set; }
-        public bool Modal { get; set; }
-        public bool Percents { get; set; }
-        public bool CloseButtonInactive { get; set; }
-        public string Icon { get; set; }
-    }
-
-    public class DialogContext
-    {
-        private readonly DialogsService _Service;
-
-        private DialogContext(DialogInfo info) {
-            Identifier = info.Identifier;
-            Top = info.Top;
-            Left = info.Left;
-            Width = info.Width;
-            Height = info.Height;
-            Header = info.Header;
-            Actions = info.Actions;
-            CancelResult = info.CancelResult;
-            Modal = info.Modal;
-            Percents = info.Percents;
-            CloseButtonInactive = info.CloseButtonInactive;
-            Icon = info.Icon;
+            ActionExecuted?.Invoke(identifier, result);
         }
 
-        public DialogContext(DialogsService service, DialogInfo info, RenderFragment childContent) : this(info) {
-            _Service = service;
-            ChildContent = childContent;
-            OnActionExecuted = _Service.OnActionExecuted;
-        }
-
-        public RenderFragment ChildContent { get; }
-        public Action<(string identifier, string result)> OnActionExecuted { get; }
-
-        public int ZIndex => 5000 + _Service.Dialogs.IndexOf(this);
-
-        public string Identifier { get; set; }
-        public int Top { get; set; }
-        public int Left { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public string Header { get; set; }
-        public (string title, string result)[] Actions { get; set; }
-        public string CancelResult { get; set; }
-        public bool Active => _Service.Dialogs.IndexOf(this) == 0;
-        public bool Modal { get; set; }
-        public bool Percents { get; set; }
-        public bool CloseButtonInactive { get; set; }
-        public string Icon { get; set; }
-    }
-
-    public abstract class DialogHostBase : ComponentBase, IDisposable
-    {
-        [Inject]
-        public DialogsService DialogsService { get; set; }
-
-        protected override void OnInitialized() {
-            base.OnInitialized();
-            DialogsService.Update += DialogsServiceOnUpdate;
-        }
-
-        private void DialogsServiceOnUpdate() {
-            StateHasChanged();
-        }
-
-        public void Dispose() {
-            DialogsService.Update -= DialogsServiceOnUpdate;
+        private void OnUpdate() {
+            Update?.Invoke();
         }
     }
 }
---
+++ BlazorComponents/EventHandlers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorComponents
+{
+    [EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+    [EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
+    public static class EventHandlers
+    {
+    }
+}
